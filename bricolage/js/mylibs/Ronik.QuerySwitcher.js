b0b8b6ec20d34fd9cb4182220c4fcb9920b6a33d
Mark Catlaano <catalano.m@gmail.com>
2012-01-30 02:01:02 -0500

    - renaming filter to QuerySwitcher since its more appropriate to what its doing

---
+++ bricolage/js/mylibs/Ronik.QuerySwitcher.js
@@ -0,0 +1,58 @@
+var Ronik = Ronik || {};
+Ronik.QuerySwitcher = (function(options){
+
+    var settings = {
+        filter: [
+            {
+                title: "All",
+                query: "childrenof:ronikdesign.com/test.html sortOrder:chronological"
+            },
+            {
+                title: "Facebook",
+                query: "childrenof:ronikdesign.com/test.html sortOrder:chronological"
+            },
+            {
+                title: "Twitter",
+                query: "childrenof:ronikdesign.com/test.html sortOrder:chronological"
+            }
+        ],
+        renderTemplate: '#filterTemplate'
+    };
+
+    function buildFilter(filters) {
+        template = Handlebars.compile($(settings.renderTemplate).html());
+        var html = template({filter: [
+                    {
+                        title: "All",
+                        query: "childrenof:ronikdesign.com/test.html sortOrder:chronological"
+                    },
+                    {
+                        title: "Facebook",
+                        query: "childrenof:ronikdesign.com/test.html sortOrder:chronological"
+                    },
+                    {
+                        title: "Twitter",
+                        query: "childrenof:ronikdesign.com/test.html sortOrder:chronological"
+                    }
+                ]});
+        $('#page header').append(html);
+    }
+
+    function bindEvents(){
+        $('[data-filter]').on('click', function(){
+            var query = $(this).attr('data-filter');
+            $.publish("/ronik/echo/search", [query]);
+        });
+    }
+
+    function init() {
+        settings = $.extend({}, settings, options);
+        buildFilter(settings.filter);
+        bindEvents();
+    }
+
+    return {
+        init: init
+    };
+
+})();
